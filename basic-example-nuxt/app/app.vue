187da5748f34b7c6c4910ae12be0eb7bb81dffbc
Andrea Binelli <binelli.andrea@gmail.com>
2025-05-02 14:53:22 +0200

    first commit

---
+++ basic-example-nuxt/app/app.vue
@@ -0,0 +1,45 @@
+<template>
+  <v-app id="inspire">
+    <v-navigation-drawer v-model="drawer">
+      <!--  -->
+    </v-navigation-drawer>
+
+    <v-app-bar>
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+
+      <v-app-bar-title>Application</v-app-bar-title>
+    </v-app-bar>
+
+    <v-main>
+      <NuxtPage />
+    </v-main>
+  </v-app>
+</template>
+
+<script
+  setup
+  lang="ts"
+>
+const drawer = ref(false)
+</script>
+
+<style>
+@import './style.css';
+@import '@vue-flow/core/dist/style.css';
+@import '@vue-flow/core/dist/theme-default.css';
+@import '@vue-flow/controls/dist/style.css';
+
+html,
+body,
+#__nuxt {
+  margin: 0;
+  height: 100%;
+}
+
+#__nuxt {
+  text-transform: uppercase;
+  font-family: 'JetBrains Mono', monospace;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+</style>
---
+++ basic-example-nuxt/app/pages/index.vue
@@ -0,0 +1,12 @@
+<script
+  lang="ts"
+  setup
+>
+//
+</script>
+
+<template>
+  <div style="height: 100%">
+    <FlowChart />
+  </div>
+</template>
